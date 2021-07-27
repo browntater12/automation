382ec1b45604423b33bf5a1be19dc55710a26bfe
browntater12 <60050747+browntater12@users.noreply.github.com>
2021-07-26 21:12:33 -0500

    Modified to run on raspberry pi with python2.7

--- sendEmail.py
+++ sendEmail.py
@@ -26,9 +26,12 @@ def email(msg):
         message.attach(part1)
 
         # Create secure connection with server and send email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(
-                sender_email, receiver_email, message.as_string()
+        #python2 doesn't support with
+        #google servers dont like linx distros playing
+        #context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL("smtp.gmail.com") #, 465, context=context)
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
             )
+        server.quit()
